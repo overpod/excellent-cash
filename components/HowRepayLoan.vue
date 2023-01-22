088c5e068b9ba0d7a4a9754bf54fef506bb7a0d2
Trofimov Alex <overpod@yandex.ru>
2023-01-22 16:15:23 +0300

    fix

--- components/HowRepayLoan.vue
+++ components/HowRepayLoan.vue
@@ -9,7 +9,9 @@
             src="/people.svg"
             alt="Личный кабинет «Отличных наличных»"
           />
-          <div :class="$style.header_title">Личный кабинет <br :class="$style.br_320_960" />«Отличных наличных»</div>
+          <div :class="$style.header_title">
+            Личный кабинет <br :class="$style.br_320_960" />«Отличных наличных»
+          </div>
         </div>
         <div :class="$style.main_text">
           Авторизуйтесь
@@ -40,21 +42,26 @@
             src="/corona.svg"
             alt="Платёжный сервис «Золотая корона»"
           />
-          <div :class="$style.header_title">Платёжный сервис <br :class="$style.br_320_960" />«Золотая корона»</div>
+          <div :class="$style.header_title">
+            Платёжный сервис <br :class="$style.br_320_960" />«Золотая корона»
+          </div>
         </div>
         <div :class="$style.main_text">
-          Перейдите
-          <a
-            :class="$style.link_main"
-            link
-            rel="stylesheet"
-            href="https://koronapay.com/repayment/offline/how-to-make/"
-            target="_blank"
-          >
-            по ссылке</a
-          >
-          и введите в строке поиска нужное юридическое лицо, указанное в вашем договоре
-          займа.<br />Выберите подходящий способ погашения и произведите платеж.
+          <p>
+            Перейдите
+            <a
+              :class="$style.link_main"
+              link
+              rel="stylesheet"
+              href="https://koronapay.com/repayment/offline/how-to-make/"
+              target="_blank"
+            >
+              по ссылке</a
+            >
+            и введите в строке поиска нужное юридическое лицо, указанное в вашем договоре
+            займа.
+          </p>
+          <p>Выберите подходящий способ погашения и произведите платеж.</p>
         </div>
       </div>
       <div :class="$style.content">
@@ -64,12 +71,19 @@
             src="/contract.svg"
             alt="По реквизитам договора займа"
           />
-          <div :class="$style.header_title">По реквизитам <br :class="$style.br_320_960" />договора займа</div>
+          <div :class="$style.header_title">
+            По реквизитам <br :class="$style.br_320_960" />договора займа
+          </div>
         </div>
         <div :class="$style.main_text">
-          В договоре займа указаны реквизиты юридического лица, на счет которого вы можете
-          перевести денежные средства в счет погашения долга. Отправьте платеж по
-          реквизитам в отделении банка, либо в банковком мобильном приложении.
+          <p>
+            В договоре займа указаны реквизиты юридического лица, на счет которого вы
+            можете перевести денежные средства в счет погашения долга.
+          </p>
+          <p>
+            Отправьте платеж по реквизитам в отделении банка, либо в банковком мобильном
+            приложении.
+          </p>
         </div>
       </div>
       <div :class="$style.content">
@@ -79,7 +93,9 @@
             src="/crown.svg"
             alt="Банковская карта любого банка"
           />
-          <div :class="$style.header_title">Банковская карта <br :class="$style.br_320_960" />любого банка</div>
+          <div :class="$style.header_title">
+            Банковская карта <br :class="$style.br_320_960" />любого банка
+          </div>
         </div>
         <div :class="$style.main_text">
           Если вы знаете, что можете забыть внести платеж по займу в нужную дату,
@@ -94,9 +110,11 @@
 <style lang="scss" module>
 .br_320_960 {
   display: block;
+
   @media (min-width: 40rem) {
     display: none;
   }
+
   @media (min-width: 60rem) {
     display: block;
   }
@@ -105,16 +123,20 @@
 .root {
   padding: 4rem 2rem;
   background-color: #fff;
+
   @media (min-width: 40rem) {
     padding: 6rem 4rem;
   }
+
   @media (min-width: 60rem) {
     padding: 9rem 4rem;
   }
+
   @media (min-width: 75rem) {
     padding: 8rem 6rem;
   }
 }
+
 .title {
   margin: 0;
   font-family: 'Circe', arial, sans-serif;
@@ -125,29 +147,36 @@
   text-align: center;
   color: #282828;
   background-color: #fff;
+
   @media (min-width: 40rem) {
     font-size: 3.6rem;
   }
+
   @media (min-width: 60rem) {
     font-size: 4.6rem;
   }
+
   @media (min-width: 75rem) {
     font-size: 5.2rem;
   }
 }
+
 .main {
   padding-top: 3rem;
   display: grid;
   grid-template-columns: 1fr;
   row-gap: 2rem;
+
   @media (min-width: 40rem) {
     padding-top: 2.4rem;
   }
+
   @media (min-width: 60rem) {
     padding-top: 5rem;
     grid-template-columns: 1fr 1fr;
     gap: 2rem;
   }
+
   @media (min-width: 75rem) {
     padding-top: 6rem;
   }
@@ -157,9 +186,11 @@
   background-color: #f6f6f6;
   border-radius: 3rem;
   padding: 2rem 1.6rem;
+
   @media (min-width: 40rem) {
     padding: 3rem 4rem;
   }
+
   @media (min-width: 60rem) {
     padding: 4rem;
   }
@@ -173,12 +204,15 @@
 .main_icon {
   width: 5rem;
   padding-right: 1rem;
+
   @media (min-width: 40rem) {
     width: 7rem;
   }
+
   @media (min-width: 60rem) {
     padding-right: 2rem;
   }
+
   @media (min-width: 75rem) {
     width: 9rem;
   }
@@ -191,12 +225,15 @@
   font-size: 2.2rem;
   line-height: 100%;
   color: #282828;
+
   @media (min-width: 40rem) {
     font-size: 2.6rem;
   }
+
   @media (min-width: 60rem) {
     font-size: 3.2rem;
   }
+
   @media (min-width: 75rem) {
     font-size: 3.2rem;
   }
@@ -210,13 +247,16 @@
   line-height: 150%;
   color: #282828;
   padding-top: 1rem;
+
   @media (min-width: 40rem) {
     padding-top: 2.8rem;
     font-size: 1.8rem;
   }
+
   @media (min-width: 60rem) {
     padding-top: 2rem;
   }
+
   @media (min-width: 75rem) {
     padding-top: 3.2rem;
   }
